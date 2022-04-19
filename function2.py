0360c7c7045cab0ef05f942aad07a0d8c5834a87
kaphowza <kaphowza@gmail.com>
2022-04-19 09:50:49 +0900

    sdf

--- function2.py
+++ function2.py
@@ -1,4 +1,8 @@
 #function2.py
+from os import times_result
+from unittest import result
+
+
 x=1
 def func(a):
     return a+x
@@ -10,3 +14,27 @@ def func2(a):
     return a+x
 
 print (func2(1))
+
+def times(a=10,b=20):
+    return a*b
+print(times())
+print(times(5))
+print(times(5,6))
+
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port
+    return strURL
+
+print(connectURI("credu.com","80"))
+print(connectURI(port="80", server="credu.com"))
+
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+print(union("HAN", "EGG"))
+print(union("HAM", "EGG", "SPAM"))
